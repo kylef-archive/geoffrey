74980b4a9b421b8f626daccfef77e82e485b89e3
Kyle Fuller <inbox@kylefuller.co.uk>
2009-03-12 09:28:40 +0000

    Updated Makefile, added a .gitignore. Added the base of gbb (geoffrey bot)

--- Makefile
+++ Makefile
@@ -10,22 +10,23 @@ ARFLAGS = rcs
 
 
 all:
+	mkdir -p bin
 	$(CC) $(CFLAGS) $(HEADERS) -c libgeoffrey/net.c -o net.o
 	$(CC) $(CFLAGS) $(HEADERS) -c libgeoffrey/main.c -o main.o
 	$(CC) $(CFLAGS) $(HEADERS) -c libgeoffrey/helpers.c -o helpers.o
 	$(AR) $(ARFLAGS) libgeoffrey.a main.o net.o helpers.o
 
 clean:
-	rm *.a *.o
+	rm -rf *.a *.o bin
 
 bot: all
 	$(CC) $(CFLAGS) $(HEADERS) -c bot/main.c -o main.o
-	$(CC) $(CFLAGS) $(LIBS) -o geoffrey main.o $(LIB)
+	$(CC) $(CFLAGS) $(LIBS) -o bin/geoffrey main.o $(LIB)
 
 examples: all
 	$(CC) $(CFLAGS) $(HEADERS) -c examples/echo.c -o echo.o
 	$(CC) $(CFLAGS) $(HEADERS) -c examples/logger.c -o logger.o
 	$(CC) $(CFLAGS) $(HEADERS) -c examples/relay.c -o relay.o
-	$(CC) $(CFLAGS) $(LIBS) -o echo echo.o $(LIB)
-	$(CC) $(CFLAGS) $(LIBS) -o logger logger.o $(LIB)
-	$(CC) $(CFLAGS) $(LIBS) -o relay relay.o $(LIB)
+	$(CC) $(CFLAGS) $(LIBS) -o bin/echo echo.o $(LIB)
+	$(CC) $(CFLAGS) $(LIBS) -o bin/logger logger.o $(LIB)
+	$(CC) $(CFLAGS) $(LIBS) -o bin/relay relay.o $(LIB)
--- bot/main.c
+++ bot/main.c
@@ -1,6 +1,45 @@
 #include <geoffrey.h>
 
+/* Geoffrey Bell Bot (GBB) */
+#define DEBUG 1
+
+typedef struct gbb_bot_struct {
+    char prefix;
+} gbb_bot;
+
+/* This function converts a privmsg signal into a command signal */
+void gbb_commandSignal(geoffrey *g, char *message, char *data) {
+    if (data[0] == ((gbb_bot *)g->info)->prefix) {
+        char *command = strtok(data, " ");
+        
+        if (command != NULL) {
+            int len = strlen(command);
+            if (len > 1) {
+                char *cmd = malloc(len + 4); /* Null (-! on command[0]) */
+                strcpy(cmd, "cmd.");
+                strcat(cmd, &command[1]);
+                
+                gb_runSignal(g, cmd, data);
+                
+                free(cmd);
+            }
+            free(command);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
-    printf("To be implemented\n");
+    gbb_bot info;
+    info.prefix = '!';
+    
+    geoffrey *g = gb_alloc();
+    gb_init(g, stderr, "irc.allshells.org", 6667, "bot", "geoffrey bot", &info);
+    gb_registerHelpers(g, DEBUG);
+    
+    gb_registerSignal(g, GB_PRIVMSG_SIG, (gb_callback*)gbb_commandSignal);
+    
+    gb_loop(g, 1);
+    gb_dealloc(g);
+    
     return 0;
 }
